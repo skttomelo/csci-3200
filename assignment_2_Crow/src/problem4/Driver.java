c3a43db67c6452e796686b99e5ffd312520183fc
Trevor <trevor.h.crow@gmail.com>
2019-01-28 15:37:36 -0500

    refactoring of formulas

--- assignment_2_Crow/src/problem4/Driver.java
+++ assignment_2_Crow/src/problem4/Driver.java
@@ -14,30 +14,38 @@ public class Driver {
 		return sum; //+1
 	}
 	
-	//formula: 1+2+(N^2)+2N+2N+N+2N, O(N^2)
+	//formula: 1+2+(N^2)+3N+2N+N+2N+1, O(N^2)
 	public static int summationTwo(int n){
-		int sum = 0;//+1
+		int sum = 0; //+1
 		//+2 for i=0 and condition prior to entering loop
-		for(int i = 0; i<n; i++){ //N^2
+		for(int i = 0; i<n; i++){ //N^2+3N
 			//2N for j=0 and condition prior to entering loop
 			for(int j = 0; j<n; j++){ //2N+N+2N
 				sum++; //2N for assignment and addition
 				//2N for j assignment and addition
 				//1N for condition
 			}
+			//2N for i assignment and addition
+			//1N for condition
 		}
 		return sum; //+1
 	}
 	
-	
+	//formula: 1+2+N^2+4N+3N+3N+2N+1, O(N^2)
 	public static int summationThree(int n){
-		int sum = 0;
-		for(int i = 0; i<n; i++){
-			for(int j = 0; j<(n*n); j++){
-				sum++;
+		int sum = 0; //+1
+		//+2 for i=0 and condition prior to entering loop
+		for(int i = 0; i<n; i++){ //N^2+4N+3N
+			//3N for J=0 and condition prior to entering loop and multiplication
+			for(int j = 0; j<(n*n); j++){ //3N+2N
+				sum++; //2N for assignment and addition
+				//2N for j assignment and addition
+				//1N for condition
 			}
+			//2N for i assignment and addition
+			//2N for condition and multiplication
 		}
-		return sum;
+		return sum; //+1
 	}
 	
 	
