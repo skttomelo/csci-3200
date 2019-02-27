d1e83f6046b221669d87b04824be718657df54d6
Trevor <trevor.h.crow@gmail.com>
2019-02-27 00:09:36 -0500

    I still can't get prefix to work

--- assignment_4_Crow/src/problem5/EquationBinaryTree.java
+++ assignment_4_Crow/src/problem5/EquationBinaryTree.java
@@ -3,171 +3,153 @@ package problem5;
 public class EquationBinaryTree {
 	private Node root;
 
-	public EquationBinaryTree()
-	{
+	public EquationBinaryTree() {
 		root = null;
 	}
 
-
-	//(left parent right)
-	//(->left->parent->right->)
-	public void printInfix()
-	{
+	// (left parent right)
+	// (->left->parent->right->)
+	public void printInfix() {
 		printInfixHelper(root);
 		System.out.println();
 	}
-	private void printInfixHelper(Node n)//O(N) - visit each node only once
+
+	private void printInfixHelper(Node n)// O(N) - visit each node only once
 	{
-		if(n != null && n.left != null)
-		{
+		if (n != null && n.left != null) {
 			System.out.print("(");
-			printInfixHelper(n.left);//left side
-			System.out.print(n.value);//middle item//parent
-			printInfixHelper(n.right);//right side
+			printInfixHelper(n.left);// left side
+			System.out.print(n.value);// middle item//parent
+			printInfixHelper(n.right);// right side
 			System.out.print(")");
-		}
-		else if(n != null)
-		{
-			System.out.print(n.value);//middle item//parent
+		} else if (n != null) {
+			System.out.print(n.value);// middle item//parent
 		}
 	}
-	//left -> right -> parent
-	public void printPostfix()
-	{
+
+	// left -> right -> parent
+	public void printPostfix() {
 		printPostfixHelper(root);
 		System.out.println();
 	}
-	private void printPostfixHelper(Node n)
-	{
-		if(n != null && n.left != null)
-		{
-			printPostfixHelper(n.left);//left side
-			printPostfixHelper(n.right);//right side
-			System.out.print(n.value);//middle item//parent
-		}
-		else if(n != null)
-		{
-			System.out.print(n.value);//middle item//parent
+
+	private void printPostfixHelper(Node n) {
+		if (n != null && n.left != null) {
+			printPostfixHelper(n.left);// left side
+			printPostfixHelper(n.right);// right side
+			System.out.print(n.value);// middle item//parent
+		} else if (n != null) {
+			System.out.print(n.value);// middle item//parent
 		}
 	}
-	//parent -> left -> right
-	public void printPrefix()
-	{
+
+	// parent -> left -> right
+	public void printPrefix() {
 		printPrefixHelper(root);
 		System.out.println();
 	}
-	private void printPrefixHelper(Node n)
-	{
-		if(n != null && n.left != null)
-		{
-			System.out.print(n.value);//middle item//parent
-			printPrefixHelper(n.left);//left side
-			printPrefixHelper(n.right);//right side
-		}
-		else if(n != null)
-		{
-			System.out.print(n.value);//middle item//parent
+
+	private void printPrefixHelper(Node n) {
+		if (n != null && n.left != null) {
+			System.out.print(n.value);// middle item//parent
+			printPrefixHelper(n.left);// left side
+			printPrefixHelper(n.right);// right side
+		} else if (n != null) {
+			System.out.print(n.value);// middle item//parent
 		}
 	}
 
-	//(a+b)
-	//(a+(b*c))
-	//((a*b)+c)
-	public void populateFromInfix(String equation)
-	{
+	// (a+b)
+	// (a+(b*c))
+	// ((a*b)+c)
+	public void populateFromInfix(String equation) { // O(N)
 		root = populateFromInfixHelper(equation);
 	}
-	public Node populateFromInfixHelper(String equation)
-	{
-		if(equation.length() == 1)
-		{
-			return new Node(equation);//math operand
+
+	public Node populateFromInfixHelper(String equation) { // O(N)
+		if (equation.length() == 1) {
+			return new Node(equation);// math operand
 		}
 
-		//System.out.println(equation);
-		String temp = equation.substring(1, equation.length()-1);//remove wrapper paren
+		// System.out.println(equation);
+		String temp = equation.substring(1, equation.length() - 1);// remove wrapper paren
 
-		//begin search for middle of equation
+		// begin search for middle of equation
 		int parenCount = 0;
 		int mid = 0;
-		for(int i = 0; i < temp.length(); i++)
-		{
-			if(temp.charAt(i) == '(')
+		for (int i = 0; i < temp.length(); i++) { //O(N-2)
+			if (temp.charAt(i) == '(')
 				parenCount++;
-			if(temp.charAt(i) == ')')
+			if (temp.charAt(i) == ')')
 				parenCount--;
-			if(parenCount == 0)
-			{
-				mid = i+1;
+			if (parenCount == 0) {
+				mid = i + 1;
 				break;
 			}
 		}
-		//middle
-		Node n = new Node(""+temp.charAt(mid));
-		//first half
+		// middle
+		Node n = new Node("" + temp.charAt(mid));
+		// first half
 		n.left = populateFromInfixHelper(temp.substring(0, mid));
 //		System.out.println(temp.substring(0, mid));
-		//second half
-		n.right = populateFromInfixHelper(temp.substring(mid+1));
+		// second half
+		n.right = populateFromInfixHelper(temp.substring(mid + 1));
 //		System.out.println(temp.substring(mid+1));
 
 		return n;
 	}
-	//ab+ = (a+b)
-	public void populateFromPostfix(String equation)
-	{
-		//complete this section for #5 from Assignment 4
-		//does not require recursion to solve
+
+	// ab+ = (a+b)
+	public void populateFromPostfix(String equation) { // O(N) best case scenario would be O(1)
+		// complete this section for #5 from Assignment 4
+		// does not require recursion to solve
 		root = populateFromPostFixHelper(equation);
 	}
-	private Node populateFromPostFixHelper(String equation) {
-		if(equation.length() == 1) {
+
+	private Node populateFromPostFixHelper(String equation) { // O(N) where N is the length of the equation
+		if (equation.length() == 1) {
 			return new Node(equation);
 		}
 		MyStack<Node> stack = new MyStack<Node>();
 		Node current;
-		
-		for(int i = 0; i<equation.length(); i++) {
-			current = new Node(""+equation.charAt(i));
-			if(equation.charAt(i) != '+' && equation.charAt(i) != '-' && equation.charAt(i) != '*' && equation.charAt(i) != '/' && equation.charAt(i) != '^') {
+
+		for (int i = 0; i < equation.length(); i++) {
+			current = new Node("" + equation.charAt(i));
+			if (equation.charAt(i) != '+' && equation.charAt(i) != '-' && equation.charAt(i) != '*' && equation.charAt(i) != '/' && equation.charAt(i) != '^') {
 				stack.push(current);
-			}else {
+			} else {
 				current.right = stack.pop();
 				current.left = stack.pop();
-				
+
 				stack.push(current);
 			}
 		}
-		
+
 		current = stack.pop();
-		
+
 		return current;
 	}
-	//+ab = (a+b)
-	public void populateFromPrefix(String equation)
-	{
-		//complete this section for #5 from Assignment 4
-		//does not require recursion to solve
-		
+
+	// +ab = (a+b)
+	public void populateFromPrefix(String equation) { // O(N) if I knew how to implement this, but as it stands without everything it's O(1)
+		// complete this section for #5 from Assignment 4
+		// does not require recursion to solve
+
 		root = populateFromPrefixHelper(equation);
 	}
+
 	private Node populateFromPrefixHelper(String equation) {
-		if(equation.length() == 1) {
-			return new Node(equation);
-		}
-		
-		for(int i = 0; i<equation.length(); i++) {
-			
+		if (equation.length() == 1) {
+			root = new Node(equation);
 		}
 		return null;
 	}
 
-	private class Node
-	{
+	private class Node {
 		String value;
 		Node left, right;
-		public Node(String v)
-		{
+
+		public Node(String v) {
 			value = v;
 			left = null;
 			right = null;
--- assignment_4_Crow/src/problem5/EquationBinaryTreeTester.java
+++ assignment_4_Crow/src/problem5/EquationBinaryTreeTester.java
@@ -5,36 +5,36 @@ public class EquationBinaryTreeTester {
 	public static void main(String[] args) {
 		EquationBinaryTree ebt = new EquationBinaryTree();
 
-		ebt.populateFromInfix("(a+b)");
-		ebt.printInfix();
-		ebt.printPostfix();
-		ebt.printPrefix();
-
-		ebt.populateFromInfix("((d/e)*f)");
-		ebt.printInfix();
-		ebt.printPostfix();
-		ebt.printPrefix();
-		/**/
-		ebt.populateFromInfix("(a+(b+c))");
-		ebt.printInfix();
-		ebt.printPostfix();
-		ebt.printPrefix();
-
-		ebt.populateFromInfix("((a+(b*c))+(((d*e)+f)*g))");
-		ebt.printInfix();
-		ebt.printPostfix();
-		ebt.printPrefix();
-		/**/
-		
-		System.out.println();
+//		ebt.populateFromInfix("(a+b)");
+//		ebt.printInfix();
+//		ebt.printPostfix();
+//		ebt.printPrefix();
+//
+//		ebt.populateFromInfix("((d/e)*f)");
+//		ebt.printInfix();
+//		ebt.printPostfix();
+//		ebt.printPrefix();
+//		/**/
+//		ebt.populateFromInfix("(a+(b+c))");
+//		ebt.printInfix();
+//		ebt.printPostfix();
+//		ebt.printPrefix();
+//
+//		ebt.populateFromInfix("((a+(b*c))+(((d*e)+f)*g))");
+//		ebt.printInfix();
+//		ebt.printPostfix();
+//		ebt.printPrefix();
+//		/**/
+//		
+//		System.out.println();
 		ebt.populateFromPrefix("++a*bc*+*defg");
 		ebt.printInfix();
 		ebt.printPostfix();
 		ebt.printPrefix();
-		ebt.populateFromPostfix("abc*+de*f+g*+");
-		ebt.printInfix();
-		ebt.printPostfix();
-		ebt.printPrefix();
+//		ebt.populateFromPostfix("abc*+de*f+g*+");
+//		ebt.printInfix();
+//		ebt.printPostfix();
+//		ebt.printPrefix();
 
 	}
 	
